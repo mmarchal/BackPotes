4fa6410ac9330bc1cbfcf705be45528e22ac54c4
Maxime <marchal.maxime@live.fr>
2019-09-02 16:15:44 +0200

    First commit

---
+++ src/main/java/maxime/back/potes/PotesApplication.java
@@ -0,0 +1,36 @@
+package maxime.back.potes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.modelmapper.ModelMapper;
+
+@SpringBootApplication
+@EnableSwagger2
+public class PotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotesApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	@Bean
+	public ModelMapper model() {
+		ModelMapper mapper = new ModelMapper();
+		return mapper;
+	}
+
+}
---
+++ src/test/java/maxime/back/potes/PotesApplicationTests.java
@@ -0,0 +1,16 @@
+package maxime.back.potes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
